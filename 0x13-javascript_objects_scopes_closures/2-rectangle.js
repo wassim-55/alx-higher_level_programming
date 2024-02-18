7779fc347c011713ff4e9443bf69e1afc1707375
wassim-55 <wchahed55@gmail.com>
2024-02-18 13:59:53 -0800

    constructor rectangle

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
 class Rectangle {
-  constructor Recatngle {
-    if ((w > 0) && (h >0)) {
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
       this.width = w;
       this.height = h;
     }
   }
 }
+
 module.exports = Rectangle;
