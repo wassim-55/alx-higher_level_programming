5b8cc93fc9c23bfff577cb541775598cbb348a13
wassim-55 <wchahed55@gmail.com>
2024-02-18 16:15:52 -0800

    reverse list

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-  let len = list.length;
-  for (let i = 0; i < len / 2; i++) {
-    const aux = list[i];
+  const len = list.length;
+  for (let i = 0; i < (len / 2); i++) {
+    let aux = list[i];
     list[i] = list[len - 1 - i];
     list[len - 1 - i] = aux;
   }
